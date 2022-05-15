767081159121328b70a46336c8efca0e955a45c3
Dunspa <jlgallego99@correo.ugr.es>
2022-05-15 18:17:47 +0200

    Limitar angulos regular

--- FPV.gd
+++ FPV.gd
@@ -1,15 +1,13 @@
 extends Spatial
 
 const SPEED = 2
-const ROTMAX_ARRIBA = -90
-const ROTMAX_ABAJO = 35
-var roty_total = 0
-var rotx_total = 0
+const ROTMAX_ARRIBA = 90
+const ROTMAX_ABAJO = -90
 
 onready var Yaw = get_parent()
 
 # Sensibilidad del ratón (para que se mueva la cámara más rápido o lento)
-const SENSIBILIDAD = 300
+export var SENSIBILIDAD = 300
 
 func _ready():
 	# Al iniciar el entorno, ocultar el ratón únicamente dentro del viewport
@@ -20,20 +18,19 @@ func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	
+	var rot_pos = SPEED * delta
+	var rot_neg = -SPEED * delta	
+	
 	# Mover la vista de la cámara con las flechas
 	if Input.is_action_pressed("Camara_derecha"):
-		roty_total += 2
-		Yaw.rotate_object_local(Vector3(0, 1, 0), -SPEED * delta)
+		Yaw.rotate_object_local(Vector3(0, 1, 0), rot_neg)
 	elif Input.is_action_pressed("Camara_izquierda"):
-		roty_total -= 2
-		Yaw.rotate_object_local(Vector3(0, 1, 0), SPEED * delta)
+		Yaw.rotate_object_local(Vector3(0, 1, 0), rot_pos)
 	
-	if Input.is_action_pressed("Camara_arriba") && rotx_total >= ROTMAX_ARRIBA:
-		rotx_total -= 2
-		rotate_object_local(Vector3(1, 0, 0), SPEED * delta)
-	elif Input.is_action_pressed("Camara_abajo") && rotx_total <= ROTMAX_ABAJO:
-		rotx_total += 2
-		rotate_object_local(Vector3(1, 0, 0), -SPEED * delta)
+	if Input.is_action_pressed("Camara_arriba") && (get_rotation_degrees()[0] + rot_pos) <= ROTMAX_ARRIBA:
+		rotate_object_local(Vector3(1, 0, 0), rot_pos)
+	elif Input.is_action_pressed("Camara_abajo") && (get_rotation_degrees()[0] + rot_neg) >= ROTMAX_ABAJO:
+		rotate_object_local(Vector3(1, 0, 0), rot_neg)
 		
 	# Trasladar la cámara con WASD
 	if Input.is_action_pressed("fpv_izquierda"):
@@ -48,7 +45,15 @@ func _process(delta):
 		
 func _input(event):
 	# Mover la cámara con el ratón
+	# La sensibilidad actúa como el delta en las teclas
 	if event is InputEventMouseMotion:
-		# La sensibilidad actúa como el delta en las teclas
-		Yaw.rotate_object_local(Vector3(0, 1, 0), event.relative.x / -SENSIBILIDAD)
-		rotate_object_local(Vector3(1, 0, 0), event.relative.y / -SENSIBILIDAD)
+		var rotx = event.relative.y / -SENSIBILIDAD
+		var roty = event.relative.x / -SENSIBILIDAD
+		var siguiente_rotx = get_rotation_degrees()[0] + rotx
+		
+		if siguiente_rotx >= ROTMAX_ABAJO && siguiente_rotx <= ROTMAX_ARRIBA:
+			rotate_object_local(Vector3(1, 0, 0), rotx)
+		#if get_rotation_degrees()[0] >= ROTMAX_ARRIBA && get_rotation_degrees()[0] <= ROTMAX_ABAJO:
+		
+			
+		Yaw.rotate_object_local(Vector3(0, 1, 0), roty)
