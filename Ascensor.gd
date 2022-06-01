896d3281b2ed9194f45edd4a1764b54e9890a263
Dunspa <jlgallego99@correo.ugr.es>
2022-06-01 13:05:19 +0200

    Boton ascensor 1

--- Ascensor.gd
+++ Ascensor.gd
@@ -9,15 +9,16 @@ var abajo = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	Global.Ascensor = self
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Ascensor"):
-		if !abajo:
-			abajo = true
-			anim.play("MoverAscensor")
-		else:
-			abajo = false
-			anim.play_backwards("MoverAscensor")
+	pass
+
+func Mover():
+	if !abajo:
+		abajo = true
+		anim.play("MoverAscensor")
+	else:
+		abajo = false
+		anim.play_backwards("MoverAscensor")
